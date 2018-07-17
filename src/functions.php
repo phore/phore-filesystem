ce5688780186c139be3b1f4e0ee0a678ad5d0ad1
matthes <leuffen@continue.de>
2018-07-17 19:11:52 +0200

    Added basic unit test

--- src/functions.php
+++ src/functions.php
@@ -10,3 +10,13 @@ function phore_uri(string $uri) : \Phore\FileSystem\PhoreUri
 {
     return new \Phore\FileSystem\PhoreUri($uri);
 }
+
+function phore_file(string $filename) : \Phore\FileSystem\PhoreFile
+{
+    return new \Phore\FileSystem\PhoreFile($filename);
+}
+
+function phore_dir(string $directory) : \Phore\FileSystem\PhoreDirectory
+{
+    return new \Phore\FileSystem\PhoreDirectory($directory);
+}
