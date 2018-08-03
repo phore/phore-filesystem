40741cb51480b74e60980a43114546dc352315f9
Matthias <m@tth.es>
2018-08-03 12:01:54 +0200

    added walk functions

--- src/PhoreDirectory.php
+++ src/PhoreDirectory.php
@@ -9,6 +9,8 @@
 namespace Phore\FileSystem;
 
 
+use Phore\FileSystem\Exception\FileAccessException;
+
 class PhoreDirectory extends PhoreUri
 {
 
@@ -19,5 +21,57 @@ class PhoreDirectory extends PhoreUri
         return $this;
     }
 
+
+    public function walk(callable $fn) : bool
+    {
+        $dir = opendir($this->path);
+        if (!$dir)
+            throw new FileAccessException("Cannot open path '$this' for indexing.");
+        while (($curSub = readdir($dir)) !== false) {
+            if ($curSub == "." || $curSub == "..")
+                continue;
+
+            $path = $this->withSubPath($dir);
+            if ($path->isFile())
+                $path = $path->asFile();
+
+            $ret = $fn($path);
+            if ($ret === false) {
+                closedir($dir);
+                return false;
+            }
+        }
+        closedir($dir);
+        return true;
+    }
+
+    public function walkR(callable $fn) : bool
+    {
+        return $this->walk(function (PhoreUri $uri) use ($fn) {
+            if ($uri->isDirectory()) {
+                return $uri->asDirectory()->walkR($fn);
+            }
+            return $fn($uri);
+        });
+    }
+
+
+    /**
+     * @return PhoreUri[]
+     * @throws FileAccessException
+     */
+    public function getListSorted() : array
+    {
+        $ret = [];
+        $this->walk(function(PhoreUri $uri) use (&$ret) {
+            $ret[] = $uri;
+        });
+        sort($ret, function (PhoreUri $a, PhoreUri $b) {
+            if ((string)$a == (string)$b)
+                return 0;
+            return ((string)$a < (string)$b) ? -1 : 1;
+        });
+        return $ret;
+    }
 
 }
