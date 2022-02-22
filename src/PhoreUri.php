1fa176d01fa1e44ee33ca78007ee119620a8ccee
Matthes <matthes@leuffen.de>
2022-02-22 23:41:27 +0100

    update

--- src/PhoreUri.php
+++ src/PhoreUri.php
@@ -323,7 +323,7 @@ class PhoreUri
         foreach ($elements as $element) {
             if ($element === "." || $element === "..")
                 throw new \InvalidArgumentException("Path security violation: path must not contain '.' or '..'");
-            $path = $path->join(addcslashes($element. "l", "\\/\n\r\t"));
+            $path = $path->join(addcslashes($element, "\\/\n\r\t"));
         }
 
         return $path;
